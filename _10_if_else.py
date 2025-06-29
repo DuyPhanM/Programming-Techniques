00dfba54ba2e9e659b906483e81b5d360b0c89b6
Duy Phan M <m.multiedu@gmail.com>
2025-06-30 01:15:23 +0700

    Create _10_if_else.py

---
+++ _10_if_else.py
@@ -0,0 +1,15 @@
+# Bài toán bán vé xem xiếc theo độ tuổi
+# Dưới 6 tuổi - Miễn phí
+# Từ 6 đến dưới 18 tuổi - 50k
+# Từ 18 tuổi trở lên - 100k
+
+age = 38
+
+if age < 6:
+    ticketPrice = 0
+elif age >= 18:
+    ticketPrice = 100000
+else:
+    ticketPrice = 50000
+
+print(f'Bạn ở độ tuổi {age}. Giá vé của bạn là {ticketPrice}đ.')
