0085f336191e38b0940d228d492fec85b80c5b8c
Duy Phan M <m.multiedu@gmail.com>
2025-06-30 01:28:27 +0700

    Create _17_library_importing.py

---
+++ _17_library_importing.py
@@ -0,0 +1,22 @@
+# Gọi trực tiếp thư viện
+# import math
+# a = math.sqrt(9)
+# p = math.pi
+
+# Gọi trực tiếp thư viện và đổi tên
+# import math as m
+# a = m.sqrt(9)
+# p = m.pi
+
+# Gọi riêng chức năng cần dùng
+from math import sqrt, pi
+a = sqrt(9)
+p = pi
+
+# Gọi riêng chức năng cần dùng và đổi tên
+# from math import sqrt as cb2, pi as bi
+# a = cb2(9)
+# p = bi
+
+print(a)
+print(p)
