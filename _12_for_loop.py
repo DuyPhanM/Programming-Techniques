c94c2978b4608cbc0afac5dba200d26ad9449883
Duy Phan M <m.multiedu@gmail.com>
2025-06-30 01:24:17 +0700

    Create _12_for_loop.py

---
+++ _12_for_loop.py
@@ -0,0 +1,22 @@
+# Ngân hàng chỉ cho thử đăng nhập 5 lần
+# để bảo mật tài khoản khách hàng
+
+accounts = [
+    {'username': 'alex', 'password': '300475'},
+    {'username': 'david', 'password': '190845'},
+    {'username': 'emily', 'password': '270173'}
+]
+
+user = 'emily'
+print('Username: ' + user)
+
+for i in range(5):
+    isValid = False
+    password = input('Password: ')
+    for acc in accounts:
+        if acc['username'] == user and acc['password'] == password:
+            isValid = True
+            break
+    if isValid == True:
+        print('Hello ' + user + '!')
+        break
