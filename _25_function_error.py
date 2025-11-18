29aee54daa7f1f66d3ddf41eb443a1cf56035ee8
Duy Phan M <m.multiedu@gmail.com>
2025-11-18 18:56:52 +0700

    Update _25_function_error.py

--- _25_function_error.py
+++ _25_function_error.py
@@ -1,19 +1,22 @@
-isValid = False
-while not isValid:
-    name = input('Nhập tên của bạn: ')
-    words = name.split()
-    if len(words) < 2:
-        continue
-    for i in range(len(words)):
-        # Đặt điều kiện không chuẩn
-        if words[i].isalpha():
+def enter_name():
+    words = []
+    isValid = False
+    while not isValid:
+        name = input('Nhập tên của bạn: ')
+        words = name.split()
+        if len(words) < 2:
+            continue
+        for i in range(len(words)):
+            # Đặt điều kiện không chuẩn
+            if words[i].isalpha():
+                break
+            if i == len(words) - 1:
+                isValid = True
+            # Đặt lệnh ngắt không hợp lý
             break
-        if i == len(words) - 1:
-            isValid = True
-        # Đặt lệnh ngắt không hợp lý
-        break
+    return words
 
-def abbreviatedName(word):
+def abbreviatedName(words):
     abb = ''
     for i in range(len(words) - 1):
         # Dùng sai phép biến đổi dữ liệu
@@ -22,4 +25,5 @@ def abbreviatedName(word):
 abb += words[-1]
     return abb
 
+name = enter_name()
 print(abbreviatedName(name))
