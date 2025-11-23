cb163a14b7c85ea340e92ca5c0d800b1b0859d36
Duy Phan M <m.multiedu@gmail.com>
2025-11-24 05:02:06 +0700

    Add comments to fib_memo function for clarity
    Added comments to explain the memoization process in the Fibonacci function.

--- _20_memoization.py
+++ _20_memoization.py
@@ -1,7 +1,11 @@
 def fib_memo(n, memo={}):
+    # 1. Điều kiện dừng khi kết quả cần tìm đã được lưu trữ
     if n in memo:
         return memo[n]
+    # 2. Điều kiện dừng khi đệ quy về giá trị khởi đầu
     if n <= 1:
         return n
+        
+    # 3. Tính toán và lưu giá trị vào danh sách
     memo[n] = fib_memo(n - 1, memo) + fib_memo(n - 2, memo)
     return memo[n]
