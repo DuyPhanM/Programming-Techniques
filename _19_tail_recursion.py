fc6dd5197f5cf95339201173a8629a098351df48
Duy Phan M <m.multiedu@gmail.com>
2025-11-24 04:53:52 +0700

    Update _19_tail_recursion.py

--- _19_tail_recursion.py
+++ _19_tail_recursion.py
@@ -1,4 +1,11 @@
 def fib_tail(n, a=0, b=1):
+    # 1. Điều kiện dừng đệ quy
     if n == 0:
         return a
+        
+    # 2. Cấu trúc tương đồng
     return fib_tail(n - 1, b, a + b)
+    
+    
+    
+    
