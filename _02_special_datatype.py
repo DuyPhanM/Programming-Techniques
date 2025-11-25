4003ebfcf8b9d3b8e4e937853167c1c0d1eb884e
Duy Phan M <m.multiedu@gmail.com>
2025-11-25 09:06:02 +0700

    Reorganize comments and variable assignments

--- _02_special_datatype.py
+++ _02_special_datatype.py
@@ -7,9 +7,21 @@ person = {'name': 'Duy', 'age': 28, 'job': 'teacher'}
 # {'name': 'Duy', 'age': 28, 'job': 'teacher'}
 
 # Kiểu set
-hobbies = {'reading', 'traveling', 'sports', 'music', 'sports'}
+hobbies = {'reading', 'traveling', 'sports', 'music'}
 # {'reading', 'traveling', 'sports', 'music'}
 
 # Kiểu list
 shopping_list = ['milk', 'eggs', 'bread', 'butter']
 # ['milk', 'eggs', 'bread', 'butter']
+
+
+# Thứ tự
+print(person['name'])
+
+# Tính bất biến
+# location[0] = 0
+# print(location)
+
+# Giá trị duy nhất
+# location = (21.4912, 103.0023, 21.4912)
+# print(location)
