dd1a6b83e6969b488e073eb7773143e5be0a1639
Duy Phan M <m.multiedu@gmail.com>
2025-11-27 02:44:33 +0700

    Update main.py

--- school_project/main.py
+++ school_project/main.py
@@ -3,10 +3,7 @@ from school.student.grade.grade import avg_grade
 from school.reward.reward import award_scholarship
 
 if __name__ == "__main__":
-    
     print("--- Ứng dụng quản lý học sinh ---")
-    
-    # Test học sinh S001 (Alice)
     student_id_1 = "HS010"
     print(f"\nKiểm tra học sinh {student_id_1}:")
     avg_1 = avg_grade(student_id_1)
