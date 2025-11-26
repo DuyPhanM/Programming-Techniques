65651b237799836a7e315819f1e096d9d4017fb0
Duy Phan M <m.multiedu@gmail.com>
2025-11-27 02:30:40 +0700

    Update _6_08_ readability.py

--- _6_08_ readability.py
+++ _6_08_ readability.py
@@ -1,1 +1,22 @@
+# Nên
+PI = 3.14
 
+radius = 12
+surface_aria = radius**2 * PI
+ 
+print(surface_aria)
+
+# Tên biến có nghĩa rõ ràng
+# Khai báo hằng số đầy đủ
+# Giãn cách hợp lý
+# Đặt tên nhất quán
+
+# Không nên
+a=12
+B=a*a*3.14
+print(B)
+
+# Tên biến không rõ nghĩa
+# Sử dụng con số ma thuật
+# Thiếu sự giãn cách hợp lý
+# Đặt tên thiếu nhất quán
