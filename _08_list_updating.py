3369368443744fbc37dc6c0a0d91a6a90082ff48
Duy Phan M <m.multiedu@gmail.com>
2025-11-27 01:50:51 +0700

    Update _08_list_updating.py

--- _08_list_updating.py
+++ _08_list_updating.py
@@ -1,16 +1,16 @@
 board = [1, 9, 4, 5]
 
 # Thay đổi giá trị theo chỉ số trong danh sách
-board[2] = 7         # board = [1, 9, 7, 5]
+board[2] = 7            # board = [1, 9, 7, 5]
 
 # Thêm phần tử vào cuối danh sách
-board.append(4)      # board = [1, 9, 7, 5, 4]
+board.append(4)         # board = [1, 9, 7, 5, 4]
 
 # Loại bỏ phần tử theo chỉ số
-remove = board.pop(4) # remove = 4, board = [1, 9, 7, 5]
+remove = board.pop(4)   # remove = 4, board = [1, 9, 7, 5]
 
 # Chèn thêm 1 phần tử
-board.insert(0, 4)   # board = [4, 1, 9, 7, 5]
+board.insert(0, 4)      # board = [4, 1, 9, 7, 5]
 
 # Thay đổi 1 phần đoạn trong danh sách
 board[0:1] = [3, 0, 4]  # board = [3, 0, 4, 1, 9, 7, 5]
