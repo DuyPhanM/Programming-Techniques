36053d2e8ec1dbd9421b80476ed98efc1d13df75
Duy Phan M <m.multiedu@gmail.com>
2025-11-26 22:22:36 +0700

    Update _02_special_datatype.py

--- _02_special_datatype.py
+++ _02_special_datatype.py
@@ -1,27 +1,40 @@
 # Kiểu tuple
 location = (21.4912, 103.0023)
-# (21.4912, 103.0023)
+'''
+- Thứ tự            : Có        location[0]
+- Bất biến          : Có        location[0] = 0
+- Giá trị duy nhất  : Không     (21.4912, 103.0023, 21.4912)
+'''
 
-# Kiểu dict
+print(location)
+
+# Kiểu dictionary
 person = {'name': 'Duy', 'age': 28, 'job': 'teacher'}
-# {'name': 'Duy', 'age': 28, 'job': 'teacher'}
+'''
+- Thứ tự            : Có        person['name']
+- Bất biến          : Không     person['name'] = 'An'
+- Giá trị duy nhất  : Khóa      {'name': 'Duy', 'age': 28, 'job': 'teacher', 'name': 'An'}
+'''
+
+print(person)
 
 # Kiểu set
 hobbies = {'reading', 'traveling', 'sports', 'music'}
-# {'reading', 'traveling', 'sports', 'music'}
+'''
+- Thứ tự            : Không     hobbies[0]
+- Bất biến          : Không     hobbies.add('art')
+- Giá trị duy nhất  : Tất cả    {'reading', 'traveling', 'sports', 'music', 'sports'}
+'''
+
+print(hobbies)
 
 # Kiểu list
-shopping_list = ['milk', 'eggs', 'bread', 'butter']
-# ['milk', 'eggs', 'bread', 'butter']
+shopping_list = ['milk', 'eggs', 'bread', 'butter', 'milk']
 
+'''
+- Thứ tự            : Có        shopping_list[0]
+- Bất biến          : Không     shopping_list[0] = 'yogurt'
+- Giá trị duy nhất  : Không     ['milk', 'eggs', 'bread', 'butter', 'milk']
+'''
 
-# Thứ tự
-print(person['name'])
-
-# Tính bất biến
-# location[0] = 0
-# print(location)
-
-# Giá trị duy nhất
-# location = (21.4912, 103.0023, 21.4912)
-# print(location)
+print(shopping_list)
